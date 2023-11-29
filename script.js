2b2d52dcead42322aeb6642e45b308abac3a5cb2
douglasmcl <douglasmacielaraujo@outlook.com>
2023-11-29 14:17:39 -0300

    trying to use localstorage

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ const task = document.querySelector('.js-list-task');
 const taskInput = document.querySelector('.js-input-task');
 const taskButton = document.querySelector('.js-button-task');
 
-enterListener();
-clickListener();
-
 taskInput.addEventListener('input', () => {
     const taskInputValue = taskInput.value;
 
@@ -21,21 +18,28 @@ function createAlert(opacity) {
     alert.style.opacity = opacity;
 }
 
-function enterListener() {
     document.addEventListener('keypress', (e) => { 
         const taskInputValue = taskInput.value;
-        if(taskInputValue.length > 0 && taskInputValue.length < 70 && e.key === 'Enter') createItem();
+        if(taskInputValue.length > 0 && taskInputValue.length < 70 && e.key === 'Enter') {
+            createItem();
+            taskInput.value = '';
+            saveTasks();
+        };
     })
-}
 
-function clickListener() {
     document.addEventListener('click', (e) => {
         const taskInputValue = taskInput.value;
-
         const el = e.target;
-        if(taskInputValue.length > 0 && taskInputValue.length < 70 && el.classList.contains('js-button-task')) createItem();
+
+        if(taskInputValue.length > 0 && taskInputValue.length < 70 && el.classList.contains('js-button-task')) {
+            createItem();
+            taskInput.value = '';
+        }
+        if(el.classList.contains('js-delete-task')) {
+            el.parentElement.remove();
+            saveTasks();
+        }
     })
-}
 
 function createItem() {
     const textInput = taskInput.value;
@@ -44,6 +48,7 @@ function createItem() {
     li.innerText = textInput;
     task.appendChild(li);
     createButton(li);
+    saveTasks();
 }
 
 function createButton(li) {
@@ -52,3 +57,28 @@ function createButton(li) {
     deleteButton.classList.add('js-delete-task', 'delete-task')
     li.appendChild(deleteButton);
 }
+
+function saveTasks() {
+    const selectTasks = task.querySelectorAll('li');
+    let arrayTasks = [];
+
+    for(let t of selectTasks) {
+        let t_text = t.innerText;
+        t_text = t_text.replace("Apagar", "")
+        arrayTasks.push(t_text);
+    }
+    
+    const tasksJSON = JSON.stringify(arrayTasks);
+    localStorage.setItem('tasks', tasksJSON);
+}
+
+function getTask() {
+    const task = localStorage.getItem('tasks');
+    const taskParse = JSON.parse(task);
+    
+    for(let t of taskParse) {
+        createItem(t)
+    }
+}
+
+getTask();
